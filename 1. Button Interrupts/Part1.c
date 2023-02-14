b91c60ea9e8903a4ee532c2ccfd9a7eb69c6d857
wilkin17 <wilkin17@students.rowan.edu>
2023-02-14 16:38:42 -0500

    Completed Part 1

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -12,7 +12,7 @@
 
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
 
-void gpioInit()
+void gpioInit();
 
 
 int main(void)
@@ -35,10 +35,14 @@ int main(void)
 
     while(1)
     {
-        if (LEDColor)
+        if (LED_Color){
+            P6OUT &= ~BIT6;                 // Set P6.6 to 0
             P1OUT ^= BIT0;                  // P1.0 = toggle
-        else
-            P6OUT &= ~BIT6;                 // Set P1.0 to 0
+        }
+        else{
+            P1OUT &= ~BIT0;                 // Set P1.0 to 0
+            P6OUT ^= BIT6;                  // P6.6 = toggle
+        }
         __delay_cycles(100000);
     }
 }
@@ -80,16 +84,16 @@ __interrupt void Port_2(void)
 {
     P2IFG &= ~BIT3;                         // Clear P1.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if (P2IN & BIT3)       // Checks if the interrupt was triggered off a rising edge.
     {
         LED_Color = 0;
-        // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES |= BIT3;     // Swaps the interrupt to check for high to low edge (falling)
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else if (!(P2IN & BIT3)) // Checks if the interrupt was triggered off a falling edge.
     {
         LED_Color = 1;
-        // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES &= ~BIT3; // Swaps the interrupt to check for low to high edge (rising)
     }
 }
 
